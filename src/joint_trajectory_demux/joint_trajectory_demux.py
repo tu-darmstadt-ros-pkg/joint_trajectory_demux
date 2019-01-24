f5424a57b405258e2877ed246c6998390c9c296c
Martin Oehler <mail@martinoehler.de>
2019-01-24 15:21:21 +0100

    discovering available controllers by calling services instead of loading a config

--- src/joint_trajectory_demux/joint_trajectory_demux.py
+++ src/joint_trajectory_demux/joint_trajectory_demux.py
@@ -1,5 +1,8 @@
 import rospy
 import trajectory_msgs.msg
+import controller_manager_msgs.srv._ListControllers
+
+LIST_CONTROLLERS_SRV_NAME = "/controller_manager/list_controllers"
 
 
 class Controller:
@@ -11,14 +14,54 @@ class Controller:
         self.next_trajectory_msg = trajectory_msgs.msg.JointTrajectory()
 
 
+def is_active(controller):
+    return controller.state == "running"
+
+
+def is_joint_trajectory_controller(controller):
+    return "JointTrajectoryController" in controller.type
+
+
+def collect_joints(controller):
+    joints = []
+    for claimed in controller.claimed_resources:
+        joints += claimed.resources
+    return joints
+
+
 class JointTrajectoryDemux:
     def __init__(self, ns, pns):
         self.ns = ns
         self.pns = pns
         self.controllers = []
-        self.load_controllers(self.pns + "controller_list")
 
-        self.trajectory_sub = rospy.Subscriber("~command", trajectory_msgs.msg.JointTrajectory, self.trajectory_cb)
+        self.trajectory_sub = rospy.Subscriber("~command", trajectory_msgs.msg.JointTrajectory, self._trajectory_cb)
+
+    def discover_controllers(self):
+        rospy.logdebug("Discovering controllers")
+        # see http://wiki.ros.org/ROS/Master_API
+        master = rospy.get_master()
+        system_state = master.getSystemState()
+        cm_services = [service for service, _provider in system_state[2][2] if LIST_CONTROLLERS_SRV_NAME in service]
+        for service in cm_services:
+            list_controllers = rospy.ServiceProxy(service, controller_manager_msgs.srv.ListControllers)
+            try:
+                resp = list_controllers()
+            except rospy.ServiceException as exc:
+                rospy.logwarn("Service did not process request: " + str(exc))
+            else:
+                for controller in resp.controller:
+                    if is_joint_trajectory_controller(controller) and is_active(controller):
+                        c = Controller()
+                        ns = service[:service.find(LIST_CONTROLLERS_SRV_NAME)]
+                        c.name = ns + "/" + controller.name
+                        c.topic = c.name + "/command"
+                        c.publisher = rospy.Publisher(c.topic, trajectory_msgs.msg.JointTrajectory, queue_size=1)
+                        c.joints = collect_joints(controller)
+                        rospy.logdebug("Added controller '" + c.name + ".")
+                        rospy.logdebug("--- topic: " + c.topic)
+                        rospy.logdebug("--- joints: " + ",".join(c.joints))
+                        self.controllers.append(c)
 
     def load_controllers(self, ns):
         controllers = rospy.get_param(ns)
@@ -37,7 +80,10 @@ class JointTrajectoryDemux:
             rospy.logdebug("--- joints: " + ",".join(c.joints))
             self.controllers.append(c)
 
-    def trajectory_cb(self, trajectory_msg):
+    def _trajectory_cb(self, trajectory_msg):
+        if len(self.controllers) == 0:
+            rospy.logwarn("Controllers have not been discovered yet.")
+            return
         # Fill next trajectory message of each controller with same trajectory points
         for c in self.controllers:
             c.next_trajectory_msg.header = trajectory_msg.header
--- src/joint_trajectory_demux/joint_trajectory_demux_node.py
+++ src/joint_trajectory_demux/joint_trajectory_demux_node.py
@@ -6,4 +6,8 @@ if __name__ == "__main__":
     rospy.init_node("joint_trajectory_demux")
 
     demux = joint_trajectory_demux.JointTrajectoryDemux("/", "~")
+    # Wait for controllers to start up
+    rospy.sleep(5)
+    # demux.load_controllers(self.pns + "controller_list")
+    demux.discover_controllers()
     rospy.spin()
